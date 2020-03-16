b5a201ffb7a1c0df4375b681093a12be66a059da
shahende <shahenda.mena@gmail.com>
2020-03-16 18:58:27 +0200

    Add project files.

---
+++ MVCAttendanceSystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCAttendanceSystem.MvcApplication" Language="C#" %>
---
+++ MVCAttendanceSystem/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCAttendanceSystem.Startup))]
+namespace MVCAttendanceSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
